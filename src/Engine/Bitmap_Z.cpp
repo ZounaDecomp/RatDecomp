d17d3b066e1c9ca7e5d9004937471f6a28c06d11
Vi <kamurojunrenka@gmail.com>
2025-04-23 23:53:44 -0500

    Bitmap Renaming

--- src/Engine/Bitmap_Z.cpp
+++ src/Engine/Bitmap_Z.cpp
@@ -17,18 +17,18 @@ Bitmap_Z::~Bitmap_Z()
 }
 
 void Bitmap_Z::Init() {
-    this->flag = 0;
+    this->Flag = 0;
     Bitmap_Z::EnableFlag(0x14);
-    this->dataPointer = 0;
-    this->palPointer = 0;
-    this->width = 0;
-    this->height = 0;
-    this->formatCopy = 0;
+    this->Datas = 0;
+    this->Palette = 0;
+    this->SizeX = 0;
+    this->SizeY = 0;
+    this->trueFormat = 0;
     this->unkSetTo4 = 4;
     this->texID = -1;
-    this->transpFormat = -1;
-    this->mipmapCount = 0;
+    this->Transp = -1;
+    this->MipmapCount = 0;
     this->precalculatedSize = 0;
-    this->paletteFormat = 3;
+    this->PalFormat = PAL_8888;
 }
   
--- src/Engine/includes/Bitmap_Z.h
+++ src/Engine/includes/Bitmap_Z.h
@@ -1,6 +1,40 @@
 #ifndef _BITMAP_Z_H_
 #define _BITMAP_Z_H_
 #include "ResourceObject_Z.h"
+#include "Types_Z.h"
+enum BmFormat_Z
+{
+    BM_4            = 0x01        ,             // 4 Bits Indexed RGB
+    BM_8            = 0x02        ,             // 8 Bits Indexed RGB
+    BM_5551         = 0x07        ,             // 16 RGBA
+    BM_565          = 0x08        ,             // 16 RGB
+    BM_4444         = 0x0A        ,             // 16 RGBA
+    BM_1555         = 0x0B        ,             //.TGA compatibility    
+    BM_8888         = 0x0C        ,             // 32 RGBA
+    BM_888          = 0x0D        ,             // 24 RGB
+    BM_CMPR         = 0x0E        ,             // S3TC
+    BM_I4A4         = 0x0F        ,             // I4A4
+    BM_I8A8         = 0x10        ,             // I4A4
+};
+
+enum PalFormat_Z
+{
+    PAL_3444        = 0x01        ,             // 16 ARGB
+    PAL_565         = 0x02        ,             // 16 RGB
+    PAL_8888        = 0x03        ,             // 32 RGBA
+    PAL_LUM         = 0x04        ,             // Luminance
+    PAL_ALPHA       = 0x05        ,             // Alpha
+    PAL_DXT1        = 0x11        ,             // DXT1 (OPAQUE)
+    PAL_DXT3        = 0x12        ,             // DXT3 (ALPHA)
+    PAL_DXT5        = 0x13        ,             // DXT5 (ALPHA)
+};
+
+enum BmTransp
+{
+    BM_NO_TRANSP    = 0,
+    BM_TRANSP_ONE   = 1,
+    BM_TRANSP       = 2
+};
 
 class Bitmap_Z : public ResourceObject_Z
 {
@@ -14,20 +48,20 @@ class Bitmap_Z : public ResourceObject_Z
         void InitBmap(S32 width, S32 height, U8 format, U8* palPointer, U8 dataPointer);
         void EnableFlag(U8 _Flag)   {Flag|=_Flag;}
     private:
-        void* dataPointer;
-        void* palPointer;
+        void* Datas;
+        void* Palette;
         S16 texID;
-        U32 width;
-        U32 height;
+        U32 SizeX;
+        U32 SizeY;
         U32 precalculatedSize;
         U8 format;
-        U8 formatCopy;
-        U8 paletteFormat;
-        U8 transpFormat;
-        U8 mipmapCount;
+        U8 trueFormat;
+        U8 PalFormat;
+        U8 Transp;
+        U8 MipmapCount;
         U8 unkSetTo4;
-        U8 flag;
-        U8 dontDelete;
+        U8 Flag;
+        U8 DontDelete;
 
 };
 #endif
