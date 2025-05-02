694d601d2fa04f4a75f88e8b074391affc398dd3
Vi <kamurojunrenka@gmail.com>
2025-05-02 00:51:20 -0500

    begin language_z

---
+++ src/Engine/Language_Z.cpp
@@ -0,0 +1,10 @@
+#include "Language_Z.h"
+#include "Name_Z.h"
+
+Name_Z CharNames[20] = {
+    Name_Z::GetID("EMPTY", 0), Name_Z::GetID("CROSS", 0), Name_Z::GetID("CIRCLE", 0), Name_Z::GetID("SQUARE", 0), Name_Z::GetID("TRIANGLE", 0),
+    Name_Z::GetID("UPDOWN", 0), Name_Z::GetID("LEFTRIGHT", 0), Name_Z::GetID("DIRPAD", 0), Name_Z::GetID("L1", 0),
+    Name_Z::GetID("L2", 0), Name_Z::GetID("R1", 0), Name_Z::GetID("R2", 0), Name_Z::GetID("L3", 0), Name_Z::GetID("R3", 0),
+    Name_Z::GetID("UP", 0), Name_Z::GetID("DOWN", 0), Name_Z::GetID("LEFT", 0), Name_Z::GetID("RIGHT", 0),
+    Name_Z::GetID("SELECT", 0), Name_Z::GetID("START", 0),
+};
